c48ad77e5d4f50be22e2a100ccd97dff1fabb98a
Shreyanshi210205 <shreyanshi.230101129@iiitbh.ac.in>
2024-03-29 15:28:03 +0530

    feat: changes in add function

--- contracts/tasktrackr.sol
+++ contracts/tasktrackr.sol
@@ -5,6 +5,7 @@ contract tasktrackr {
     struct TodoItem {
         string task;
         bool isCompleted;
+        uint256 time;
     }
 
     event TaskCompleted(uint256 indexed id);
@@ -24,8 +25,9 @@ contract tasktrackr {
         _;
     }
 
-    function addTask(string calldata task) public {
-        TodoItem memory item = TodoItem({task: task, isCompleted: false});
+    function addTask(string calldata task ) public 
+    {
+        TodoItem memory item = TodoItem({task: task, isCompleted: false, time:block.timestamp});
         list[count] = item;
         count++;
     }
@@ -50,25 +52,28 @@ contract tasktrackr {
         emit ContractDestroyed(msg.sender);
         selfdestruct(payable(owner));
     }
-}
 
-    function displayAllTasks() public view returns (string[] memory tasks, bool[] memory statuses) {
+
+    function displayAllTasks() public view returns (string[] memory tasks, bool[] memory statuses,uint256[] memory times) {
     string[] memory taskList = new string[](count);
     bool[] memory statusList = new bool[](count);
+    uint256[] memory timeList = new uint256[](count);
     if(count==0)
     revert("Nothing to display");
     for (uint256 i = 0; i < count; i++) {
         TodoItem memory item = list[i];
         taskList[i] = item.task;
         statusList[i] = item.isCompleted;
+        timeList[i] = item.time;
+        
     }
 
-    return (taskList, statusList);
+    return (taskList, statusList, timeList);
 }
-    function displayById(uint256 id) public view returns (string memory task, bool isCompleted) {
+    function displayById(uint256 id) public view returns (string memory task, bool isCompleted,uint256 time) {
     require(id < count, "Task with given ID does not exist");
     TodoItem memory item = list[id];
-    return (item.task, item.isCompleted);
+    return (item.task, item.isCompleted, item.time);
     }
     
 }
