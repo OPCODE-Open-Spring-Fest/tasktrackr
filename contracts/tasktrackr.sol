2448f73950147ef411f13d3c01442441076c67e3
Jagpreet Singh Khurana <119691733+Jagpreet153@users.noreply.github.com>
2024-03-30 13:49:13 +0530

    feat: added address functionality

--- contracts/tasktrackr.sol
+++ contracts/tasktrackr.sol
@@ -1,58 +1,49 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.7.0<0.9.0;
-//mapping is to be used
+
 contract tasktrackr{
     struct TodoItem {
         string task;
         bool isCompleted;
-        uint256 time;
-    }
-    event TaskCompleted(uint256 indexed id);
-    event TaskUpdated(uint256 indexed id, string newTask);
-
-    mapping( uint256=> TodoItem) public list;
-
-    uint256 public count=0;
-    constructor()
-    {
-
+        address owner;
     }
 
-    function addTask(string calldata task) public 
-    {
-        TodoItem memory item = TodoItem({task: task, isCompleted:false, , time: block.timestamp});
-        list[count]=item;
-        count++;
+    event TaskCompleted(address indexed owner, uint256 indexed id);
+    event TaskUpdated(address indexed owner, uint256 indexed id, string newTask);
+
+    mapping(address => mapping(uint256 => TodoItem)) public list;
+    mapping(address => uint256) public count;
+
+    function addTask(string calldata task) public {
+        TodoItem memory item = TodoItem({task: task, isCompleted: false, owner: msg.sender});
+        list[msg.sender][count[msg.sender]] = item;
+        count[msg.sender]++;
     }
-    function completeTask(uint256 id) public
-    {
-        if(!list[id].isCompleted){
-            list[id].isCompleted=true;
-            emit TaskCompleted(id);
+
+    function completeTask(uint256 id) public {
+        require(msg.sender == list[msg.sender][id].owner, "Only the owner can complete the task");
+        if(!list[msg.sender][id].isCompleted){
+            list[msg.sender][id].isCompleted = true;
+            emit TaskCompleted(msg.sender, id);
         }
     }
 
-    function updateTask(uint256 id, string calldata newTask) public 
-    {
-        require(id < count, "Task with given ID does not exist");
-        list[id].task = newTask;
-        emit TaskUpdated(id, newTask);
+    function updateTask(uint256 id, string calldata newTask) public {
+        require(msg.sender == list[msg.sender][id].owner, "Only the owner can update the task");
+        list[msg.sender][id].task = newTask;
+        emit TaskUpdated(msg.sender, id, newTask);
     }
 
-    function displayAllTasks() public view returns (string[] memory tasks, bool[] memory statuses,uint[] memory times) {
-    string[] memory taskList = new string[](count);
-    bool[] memory statusList = new bool[](count);
-    uint256[] memory timeList = new uint256[](count);
-    if(count==0)
-    revert("Nothing to display");
-    for (uint256 i = 0; i < count; i++) {
-        TodoItem memory item = list[i];
-        taskList[i] = item.task;
-        statusList[i] = item.isCompleted;
-        timeList[i] = item.time;
-    }
+    function displayAllTasks() public view returns (string[] memory tasks, bool[] memory statuses) {
+        string[] memory taskList = new string[](count[msg.sender]);
+        bool[] memory statusList = new bool[](count[msg.sender]);
 
-    return (taskList, statusList,timeList);
-}
-    
+        for (uint256 i = 0; i < count[msg.sender]; i++) {
+            TodoItem memory item = list[msg.sender][i];
+            taskList[i] = item.task;
+            statusList[i] = item.isCompleted;
+        }
+
+        return (taskList, statusList);
+    }
 }
